b816d7a8df047fd09a8bdf99afb3d60ab74a6189
Rubiel Leal <lealsoft@hotmail.com>
2024-12-15 11:52:08 -0500

    dar de baja

--- conexion.php
+++ conexion.php
@@ -1,10 +1,10 @@
 <?php
 $bd_driver = "mysql";
 $bd_servidor = "localhost";
-$bd_base = "bd_cronhis";
+$bd_base = "bd_cronhis_af";
 $charset = "charset=utf8";
 $bd_usuario = "root";
-$bd_clave = "123456"; 
+$bd_clave = "12345"; 
 $_SESSION['urlin'] = "/project";
 
 $ruta_firmas='/proyecto/hc/img/firmas/';
